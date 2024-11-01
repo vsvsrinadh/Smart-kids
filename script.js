46979b87c28931861c6c6b5a5f21c8a5f0a6ba65
vsvsrinadh <srinadhveeravalli123@gmail.com>
2024-11-01 19:20:55 +0530

    pushed

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById('chatBox').style.display = 'flex';
+};
+
+function closeChat() {
+    document.getElementById('chatBox').style.display = 'none';
+}
